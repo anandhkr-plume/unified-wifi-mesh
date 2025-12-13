d20c075006a422c31d945792b929ce2610947e10
Amalesh Nandh KR <anandhkr@plume.com>
2025-12-13 13:14:52 +0530

    Update decode macro

--- src/ctrl/em_ctrl.cpp
+++ src/ctrl/em_ctrl.cpp
@@ -930,7 +930,6 @@ void em_ctrl_t::io(void *data, bool input)
 #define MAX_PARAM_LEN 64
 bus_error_t validate_ssid_input_data (cJSON *input_data, const char *input_name) {
     cJSON *item = NULL;
-    bool suiteselector = false;
     em_printfout("%s:%d AUTOCONFIG_DEBUG validating input_data:%s input_name:%s\n", __func__, __LINE__, input_data->string, input_name);
 
     if(strncmp(input_name, "SSID", strlen("SSID")) == 0) {
@@ -972,18 +971,15 @@ bus_error_t validate_ssid_input_data (cJSON *input_data, const char *input_name)
             return bus_error_invalid_input;
         }
         cJSON_ArrayForEach(item, input_data) {
-            if(strncmp(item->valuestring, "SuiteSelector", strlen("SuiteSelector")) == 0) {
-                suiteselector = true;
-                continue;
-            }
             if(!cJSON_IsString(item) || !(strncmp(item->valuestring, "psk", strlen("psk")) == 0 || strncmp(item->valuestring, "dpp", strlen("dpp")) == 0 || 
                 strncmp(item->valuestring, "sae", strlen("sae")) == 0 || strncmp(item->valuestring, "psk+sae", strlen("psk+sae")) == 0 || 
-                strncmp(item->valuestring, "dpp+sae", strlen("dpp+sae")) == 0 || strncmp(item->valuestring, "dpp+psk+sae", strlen("dpp+psk+sae")) == 0)) {
+                strncmp(item->valuestring, "dpp+sae", strlen("dpp+sae")) == 0 || strncmp(item->valuestring, "dpp+psk+sae", strlen("dpp+psk+sae")) == 0) ||
+                (strncmp(item->valuestring, "SuiteSelector", strlen("SuiteSelector")) == 0)) {
                 em_printfout("%s:%d AUTOCONFIG_DEBUG Band must be string with one or many of these values: 2.4GHz, 5GHz or 6GHz\n", __func__, __LINE__);
                 return bus_error_invalid_input;
             }
         }
-    } else if(strncmp(input_name, "SuiteSelector", strlen("SuiteSelector")) == 0 && suiteselector) {
+    } else if(strncmp(input_name, "SuiteSelector", strlen("SuiteSelector")) == 0) {
         if(!cJSON_IsString(input_data)) {
             em_printfout("%s:%d AUTOCONFIG_DEBUG SuiteSelector must be an array\n", __func__, __LINE__);
             return bus_error_invalid_input;
@@ -1064,7 +1060,11 @@ bus_error_t validate_ssid_input_data (cJSON *input_data, const char *input_name)
         char *print_input_value = cJSON_Print(input_value); \
         em_printfout("%s:%d AUTOCONFIG_DEBUG print_input_value:%s \n", __func__, __LINE__, print_input_value); \
         free(print_input_value); \
-        return validate_ssid_input_data(input_value, input_key); \
+        int ret = validate_ssid_input_data(input_value, input_key); \
+        if(ret != bus_error_success) { \
+            cJSON_Delete(input_json); \
+            return ret; \
+        } \
     } \
 } \
 
