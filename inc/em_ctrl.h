a61d687d56debbe6bf5bd672ea7e103582d0be31
Amalesh Nandh KR <anandhkr@plume.com>
2025-12-17 00:28:00 +0530

    Move SetSSID to TR-181_SetSSID and invoke method callback

--- inc/em_ctrl.h
+++ inc/em_ctrl.h
@@ -683,7 +683,12 @@ public:
 	* @param[i] output pointer to send dta
 	* @param[i] user_data get the bus handle
 	*/
-	static bus_error_t ctrl_cmd_ssid_set (char *event_name, raw_data_t *p_data, bus_user_data_t *user_data);
+	bus_error_t ctrl_cmd_ssid_set_outer(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data);
+	bus_error_t ctrl_cmd_ssid_set(char *event_name, raw_data_t *p_data, bus_user_data_t *user_data);
+
+
+	bus_error_t ctrl_cmd_ssid_set_method(char const* methodName, raw_data_t *inParams, raw_data_t *outParams, void *asyncHandle);
+	bus_error_t ctrl_cmd_ssid_set_inner(char const* methodName, raw_data_t *inParams, raw_data_t *outParams, void *asyncHandle);
 	//static bus_error_t cmd_setssid (const char *event_name, raw_data_t *inParams, raw_data_t *outParams, void *user_data);
 
 	/**!
